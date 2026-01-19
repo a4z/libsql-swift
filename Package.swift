39a160892c8ba7d07c882ccd173884dc94556d0b
a4z <harald.achitz@gmail.com>
2026-01-19 14:55:07 +0100

    supress linke searchpath warning

--- Package.swift
+++ Package.swift
@@ -6,18 +6,28 @@ import PackageDescription
 
 let libsqlDependencies: [Target.Dependency] = ["CLibsql"]
 
+// SwiftPM passes -F (framework search path) to clang on Linux even though frameworks
+// don't exist there. This causes a warning. We suppress it with this linker setting.
 #if os(Linux)
-let binaryDependencyPath: String = {
-    #if arch(x86_64)
-    return "Turso/CLibsqlLinux-x86_64-unknown-linux-gnu.artifactbundle.zip"
-    #elseif arch(arm64)
-    return "Turso/CLibsqlLinux-aarch64-unknown-linux-gnu.artifactbundle.zip"
-    #else
-    fatalError("Unsupported Linux architecture.")
-    #endif
-}()
+    let linkerSettings: [LinkerSetting] = [
+        .unsafeFlags(["-Xclang-linker", "-Wno-unused-command-line-argument"])
+    ]
 #else
-let binaryDependencyPath = "Turso/CLibsql/CLibsql.xcframework"
+    let linkerSettings: [LinkerSetting] = []
+#endif
+
+#if os(Linux)
+    let binaryDependencyPath: String = {
+        #if arch(x86_64)
+            return "Turso/CLibsqlLinux-x86_64-unknown-linux-gnu.artifactbundle.zip"
+        #elseif arch(arm64)
+            return "Turso/CLibsqlLinux-aarch64-unknown-linux-gnu.artifactbundle.zip"
+        #else
+            fatalError("Unsupported Linux architecture.")
+        #endif
+    }()
+#else
+    let binaryDependencyPath = "Turso/CLibsql/CLibsql.xcframework"
 #endif
 
 let clibsqlTarget: Target = .binaryTarget(
@@ -43,64 +53,69 @@ var package = Package(
 
         ),
         clibsqlTarget,
-        .testTarget(name: "LibsqlTests", dependencies: ["Libsql"]),
+        .testTarget(name: "LibsqlTests", dependencies: ["Libsql"], linkerSettings: linkerSettings),
 
         // Examples
         .executableTarget(
             name: "Query",
             dependencies: ["Libsql"],
-            path: "Examples/Query"
+            path: "Examples/Query",
+            linkerSettings: linkerSettings
         ),
         .executableTarget(
             name: "Transaction",
             dependencies: ["Libsql"],
-            path: "Examples/Transaction"
+            path: "Examples/Transaction",
+            linkerSettings: linkerSettings
         ),
         .executableTarget(
             name: "Batch",
             dependencies: ["Libsql"],
             path: "Examples/Batch",
-            exclude: ["README.md"]
+            exclude: ["README.md"],
+            linkerSettings: linkerSettings
         ),
         .executableTarget(
             name: "Local",
             dependencies: ["Libsql"],
             path: "Examples/Local",
-            exclude: ["README.md",]
+            exclude: ["README.md"],
+            linkerSettings: linkerSettings
         ),
         .executableTarget(
             name: "Memory",
             dependencies: ["Libsql"],
             path: "Examples/Memory",
-            exclude: ["README.md"]
+            exclude: ["README.md"],
+            linkerSettings: linkerSettings
         ),
         .executableTarget(
             name: "Remote",
             dependencies: ["Libsql"],
             path: "Examples/Remote",
-            exclude: [
-                "README.md", 
-            ]
+            exclude: ["README.md"],
+            linkerSettings: linkerSettings
         ),
         .executableTarget(
             name: "Sync",
             dependencies: ["Libsql"],
             path: "Examples/Sync",
-            exclude: [
-                "README.md", 
-            ]
+            exclude: ["README.md"],
+            linkerSettings: linkerSettings
         ),
         .executableTarget(
             name: "Transactions",
             dependencies: ["Libsql"],
             path: "Examples/Transactions",
-            exclude: ["README.md",]
+            exclude: ["README.md"],
+            linkerSettings: linkerSettings
         ),
         .executableTarget(
             name: "Vector",
             dependencies: ["Libsql"],
             path: "Examples/Vector",
-            exclude: ["README.md"]
+            exclude: ["README.md"],
+            linkerSettings: linkerSettings
         ),
     ]
 )
